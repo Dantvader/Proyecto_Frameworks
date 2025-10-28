dbd976522314d523a319020f1f125c0f7592fc33
Dantvader <164093085+Dantvader@users.noreply.github.com>
2025-10-28 10:43:45 -0600

    Use environment variables (dotenv) and add password hashing (bcrypt); add register route and .env.example

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
-const mysql = require("mysql")
-const util = require("util")
+require('dotenv').config();
+const mysql = require('mysql');
+const util = require('util');
 
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "listado"
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'listado'
 });
 
 pool.query = util.promisify(pool.query);
