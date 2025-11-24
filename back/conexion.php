64c36376b83fa0ccdf0d68af7f57c98ff6ce326b
whither64 <arvizuhuertaig@gmail.com>
2025-11-23 20:48:37 -0600

    Estoy Cansado Gfe

--- back/conexion.php
+++ back/conexion.php
@@ -1,11 +1,11 @@
 
 <?php
 // --- DATOS QUE DEBE RELLENAR ---
-$servidor_ip = "192.168.100.128";  // Ejemplo: "192.168.1.10"
+$servidor_ip = "192.168.100.128";  // Ejemplo: "192.168.1.10" de la vm en este caso
 $nombre_bd   = "base_becas";  // Ejemplo: "proyecto_web_bd"
 $usuario_bd  = "Admin1";
 $password_bd = "12345";
-$puerto      = 3306; // El puerto que configuraste
+$puerto      = 3306; // El puerto que configuraste del mysql
 
 // DSN (Data Source Name)
 $dsn = "mysql:host=$servidor_ip;port=$puerto;dbname=$nombre_bd;charset=utf8mb4";
